9c9e65c5b88bf72e742b7ce321a8b709bc9ab1cd
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-08-31 17:46:44 +0600

    upload

--- hello.js
+++ hello.js
@@ -1,6 +1,3 @@
-var number1 = 0.1;
-var number2 = 0.2;
-number1 = number1;
-var total = number1 + number2;
-total = total.toFixed(5)
-console.log(total)
+var number = -5;
+var absoluteNumbeer = Math.abs(number);
+console.log(absoluteNumbeer);
