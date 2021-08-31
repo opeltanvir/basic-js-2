030e79a2ad7254db3888ab8cf4154e1424c5adb6
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-01 00:01:27 +0600

    upload

---
+++ array.js
@@ -0,0 +1,34 @@
+var friendesAge = [15, 17, 14, 16];
+
+
+
+var sonaliAge = friendesAge[2];
+
+friendesAge[1] = 21;
+
+var position = friendesAge.indexOf(141);
+
+console.log(friendesAge);
+friendesAge.push(15);
+friendesAge.push(19);
+
+console.log(friendesAge);
+
+friendesAge.pop();
+console.log(friendesAge);
+
+
+var teaLine = ['kalam', 'salam', 'balam'];
+teaLine.push('jalam');
+console.log(teaLine);
+teaLine.pop();
+console.log(teaLine);
+teaLine.shift();
+console.log(teaLine);
+teaLine.unshift('palam');
+console.log( teaLine);
+
+
+
+
+
