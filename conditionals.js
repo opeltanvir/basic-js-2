5a9471846f30635a6ec7c58921a9e71a911c869f
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-08-31 18:57:12 +0600

    upload

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,2 @@
-var jobPaise = false;
-var savingAmount = 500000;
-
-if(jobPaise == true && savingAmount > 2000000){
-    console.log("Aso tomer patri Khuji");
-}
-else if (jobPaiso == true){
-    console.log("acha koi din pore dekhbo");
-}
-else{
-    console.log("Tor kopale biya nai");
-
-}
+var date = new Date('1971-12-16');
+console.log(date);
